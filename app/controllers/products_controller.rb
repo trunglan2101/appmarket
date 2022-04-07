06bfa7259a3e48be6a03f0c0874852bcb6f4da8f
“trunglan2101” <trunglan2101@gmail.com>
2022-04-07 20:04:57 +0700

    search&sort

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,25 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :edit, :new, :update]
-  PER_PAGE = 2
+
   def index
     @categories = Category.all
     
     cate = params[:cate]
-
+    @q = Product.ransack(params[:q])
+  
     if !cate.nil?
-      @products = Product.where(:category_id => cate).page(params[:page])
-
+      @products = Product.where(:category_id => cate)
+      @products = Kaminari.paginate_array(@products).page(params[:page])
+    elsif !@q.nil?
+      @products = @q.result(distinct: true)
+      @products = Kaminari.paginate_array(@products).page(params[:page]).per(1)
     else
       @products = Product.page(params[:page]).order("created_at desc")
-
     end
-
-    # @q = Product.ransack(params[:q])
-    # @products = @q.result(distinct: true)
-    #  @q = Product.ransack(params[:q]) 
-    # @products = @q.result.page(params[:page])
   end
 
   # def search
-  #   @products = Product.where("name LIKE ?", "%" + params[:q] + "%")
+  #   @products = Product.where("name LIKE ?", "%#{params[:q]}%")
   #   # binding.pry
   # end
 
